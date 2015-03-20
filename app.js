680e22da5f38c966b91f110b13574a02aeb89063
Ilya Isaev <isaev.ilya.1994@gmail.com>
2015-03-20 19:13:26 +0300

    minor

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var engines = require('consolidate');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
