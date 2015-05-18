ed262286c720689576be11a21d3f1532bb310e2e
isae <isaev.ilya.1994@gmail.com>
2015-05-18 19:33:26 +0300

    minor

--- deploy_remote.sh
+++ deploy_remote.sh
@@ -3,8 +3,7 @@ address="188.166.89.156"
 user="root"
 rsync -avz  \
            ./* ${user}@isae.me:~/scratchduino-blockly
-ssh "${user}@${address}" << EOF
-tmux attach
+ssh "${user}@${address}" -t tmux a << EOF
 cd scratchduino-blockly
 lsof -t -i:3000 | xargs kill -9
 ./run.sh &
